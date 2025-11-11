078c59352890c7534fc0ff0d9defab8ae2843b65
Adam Cohen <adamcohen0729@gmail.com>
2025-11-11 02:08:50 -0500

    setup and ifp files created

---
+++ sarpy_plus/ifp.py
@@ -0,0 +1,19 @@
+from sarpy_plus import RadarParams
+
+# Range Doppler Algorithm
+
+
+
+# Range Migration Algorithm / Omega K Algorithm
+
+
+
+# Chirp Scaling Algorithm
+
+
+
+# Polar Format Algorithm
+
+
+
+# Back Projection Algorithm
---
+++ sarpy_plus/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sarpy-plus",
+    version="0.1.0",
+    description="Modular Python SAR simulation and processing library (with JAX)",
+    author="Adam Cohen",
+    packages=find_packages(),
+    install_requires=[
+        "jax>=0.4.20",
+        "jaxlib>=0.4.20",
+        "matplotlib>=3.5.0",
+    ],
+    python_requires=">=3.8",
+)
